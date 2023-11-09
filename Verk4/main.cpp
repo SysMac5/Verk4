92d08f56fbd3a1d306cc34233e885cb84020d344
Maggi <sigurthor.maggi@gmail.com>
2023-11-09 22:04:56 +0000

    nei

--- Verk4/main.cpp
+++ Verk4/main.cpp
@@ -12,12 +12,17 @@
 
 // Includes
 #include <stdio.h>
+#include <stdlib.h>
 #include <pico/stdlib.h>
 #include <pico/multicore.h>
+#include <pico/util/queue.h>
 #include <hardware/i2c.h>
 #include <BME280.h>
 
 // Defines
+#define HUMI_DIFF 0.5 // Typical RMS :    0.02-0.07  %RH
+#define TEMP_DIFF 0.1 //    noise at :  0.002-0.005  °C
+#define PRES_DIFF 1.5 //        25°C :      0.2-1.3  Pa
 
 // Pinouts
 const uint8_t PIN_QT_SDA = 2;
@@ -27,11 +32,20 @@ const uint8_t PIN_QT_SCL = 3;
 BME280 bme;
 
 // Global variables and data structures
-BME280::BME_Measurement_t bme_measurement_in;
-BME280::BME_Measurement_t bme_measurement_out;
+BME280::BME_Measurement_t bme_measurement_bme280;
+BME280::BME_Measurement_t bme_measurement_core1;
+BME280::BME_Measurement_t bme_measurement_core0;
 queue_t bme_queue;
 
 // Forward declarations
+
+bool has_changed() {
+    return (
+        abs(bme_measurement_bme280.humidity - bme_measurement_core1.humidity) > HUMI_DIFF ||
+        abs(bme_measurement_bme280.temperature - bme_measurement_core1.temperature) > TEMP_DIFF ||
+        abs(bme_measurement_bme280.pressure - bme_measurement_core1.pressure) > PRES_DIFF
+    );
+}
 
 void core_1_entry() {
     i2c_init(i2c1, 400000);
@@ -46,10 +60,12 @@ void core_1_entry() {
         //printf("Initialized.\n"); // Uncomment for debugging
     }
     while (1) {
-        if (bme.read(&bme_measurement_in)) {
+        if (bme.read(&bme_measurement_bme280)) {
             // Push the measurement to core 0 for displaying
-            /*ADD YOUR CODE HERE*/
-            queue_add_blocking(&bme_queue, &bme_measurement_in);
+            if (has_changed()) {
+                bme_measurement_core1 = bme_measurement_bme280;
+                queue_add_blocking(&bme_queue, &bme_measurement_core1);
+            }
         }
         sleep_ms(10);
     }
@@ -66,11 +82,10 @@ void init() {
 
 void loop() {
     // Receive data from core 1 and display
-    /*ADD YOUR CODE HERE*/
-    queue_remove_blocking(&bme_queue, &bme_measurement_out);
-    printf("        Hitastig: %.2f °C\n", bme_measurement_out.temperature);
-    printf("  Loftþrýstingur: %.2f hPa\n", bme_measurement_out.temperature);
-    printf("        Rakastig: %.2f%%\n\n", bme_measurement_out.temperature);
+    queue_remove_blocking(&bme_queue, &bme_measurement_core0);
+    printf("        Hitastig: %.2f °C\n", bme_measurement_core0.temperature);
+    printf("  Loftþrýstingur: %.2f hPa\n", bme_measurement_core0.pressure);
+    printf("        Rakastig: %.2f%%\n\n", bme_measurement_core0.humidity);
     sleep_ms(10);
 }
 
